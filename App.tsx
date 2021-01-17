80c27893c6ee82168e72ac6c1bd6e637ac1b54c4
mauricio <movaldivia@uc.cl>
2021-01-17 14:08:42 -0300

    feat: first commit

---
+++ App.tsx
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+
+const App = () => {
+  return (
+    <>
+      <View>
+        <Text>Hola mundo</Text>
+      </View>
+    </>
+  );
+};
+
+export default App;
---
+++ readme.md
@@ -0,0 +1,19 @@
+# Setup
+
+npm install
+
+# Setup hardware
+
+Revisar esta guía: https://reactnative.dev/docs/environment-setup
+
+# Librerías:
+
+Se parte de 
+
+`npx react-native init AwesomeTSProject --template react-native-template-typescript`
+
+- redux-tools (redux): https://redux-toolkit.js.org/ -> manejo de estado
+
+- react navigation: https://reactnavigation.org/ -> navegación
+
+- axios: https://github.com/axios/axios -> llamados a la api
